a725b9c10bbc084ebd80d19b823adb014a1d5b87
Niño Piña <ninopina10@gmail.com>
2024-12-02 17:02:31 -0600

    Actualizar ComprarBot.js

--- plugins/ComprarBot.js
+++ plugins/ComprarBot.js
@@ -1,15 +1,16 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let text_owner = "〔 ISITA - BOT 〕
+const handler = async (m, {conn}) => {
+  m.reply(global.ComprarBot, img);
+};
+handler.command = /^(ComprarBot|Comprar|comprar|ComprarBot)$/i;
+export default handler;
+
+let img = 'https://i.ibb.co/s9N9QhG/file.jpg'
+global.ComprarBot = `
+〔 ISITA - BOT 〕
 
 BOT PARA GRUPO :
 wa.me/529831715910
 
 BOT PERZONALIZADO :
-wa.me/50557865603"
-await conn.sendFile(m.chat, "https://i.ibb.co/s9N9QhG/file.jpg", 'thumbnail.jpg', txt_owner, m, null, rcanal)
-}
-handler.help = ['comprarbot']
-handler.tags = ['main']
-handler.command = ['comprar', 'combot', 'comprarbot', 'botcom'] 
-
-export default handler
+wa.me/50557865603
+`;
