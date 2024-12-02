0cbc0074ea9ec18665d8febe5c214572b58e9cb4
Niño Piña <ninopina10@gmail.com>
2024-12-02 13:36:42 -0600

    Actualizar plugins

---
+++ plugins/tools-tourl.js
@@ -0,0 +1,47 @@
+import fs from 'fs'
+import FormData from 'form-data'
+import axios from 'axios'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn }) => {
+
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+
+  await m.react('🕒')
+  if (!mime.startsWith('image/')) {
+    return m.reply('Responde a una *Imagen.*')
+  }
+
+  let media = await q.download()
+  let formData = new FormData()
+  formData.append('image', media, { filename: 'file' })
+
+  let api = await axios.post('https://api.imgbb.com/1/upload?key=10604ee79e478b08aba6de5005e6c798', formData, {
+    headers: {
+      ...formData.getHeaders()
+    }
+  })
+
+  await m.react('✅')
+  if (api.data.data) {
+    let txt = '`I B B  -  U P L O A D E R`\n\n'
+        txt += `*⚜️ TÍTULO* : ${q.filename || 'x'}\n`
+        txt += `*⚜️ ID* : ${api.data.data.id}\n`
+        txt += `*⚜️ ENLACE* : ${api.data.data.url}\n`
+        txt += `*⚜️ DIRECTO* : ${api.data.data.url_viewer}\n`
+        txt += `*⚜️ MIME* : ${mime}\n`
+        txt += `*⚜️ FILE* : ${q.filename || 'x.jpg'}\n`
+        txt += `*⚜️ EXTENSION* : ${api.data.data.image.extension}\n`
+        txt += `*⚜️ DELETE* : ${api.data.data.delete_url}\n\n`
+        txt += `*➤ By: {botname}*`
+    await conn.sendFile(m.chat, api.data.data.url, 'ibb.jpg', txt, m, null, fake)
+  } else {
+    await m.react('✅')
+  }
+}
+handler.tags = ['convertir']
+handler.help = ['toibb']
+handler.command = /^(tourl|toibb)$/i
+handler.register = true 
+export default handler
