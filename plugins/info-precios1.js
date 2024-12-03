38ac27f4a1b3dfcc5df22b5d0a6cfbd23ae042b1
Niño Piña <ninopina10@gmail.com>
2024-12-03 13:33:02 -0600

    Actualizar info-precios1.js

--- plugins/info-precios1.js
+++ plugins/info-precios1.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
 // Reacción al mensaje
 m.react('🫦');
 // Comprobamos si el comando es .precios1
-if (m.body === '.precios1') {
+if (m.body.trim() === '.precios1') { // Usamos trim para eliminar espacios en blanco
 // Reemplaza la URL con la imagen que deseas enviar
 const imageUrl = 'https://i.ibb.co/TcTRpHW/file.jpg';
 // Enviamos solo la imagen
