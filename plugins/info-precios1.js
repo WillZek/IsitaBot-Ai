3da3213fdd9a4b0ae63b726cc746e3a7dba04a96
Niño Piña <ninopina10@gmail.com>
2024-12-03 13:30:14 -0600

    Actualizar info-precios1.js

--- plugins/info-precios1.js
+++ plugins/info-precios1.js
@@ -2,21 +2,18 @@
 import fs from 'fs';
 import path from 'path';
 let handler = async (m, { conn }) => {
-// Obtenemos el nombre del usuario que envía el comando
-let name2 = conn.getName(m.sender); // Nombre del usuario que envía el comando
-m.react('🫦'); // Reacción al mensaje
+// Reacción al mensaje
+m.react('🫦');
 // Comprobamos si el comando es .precios1
 if (m.body === '.precios1') {
-// Mensaje que se enviará
-let str = `\`${name2}\` pidió precios.`; // Mensaje sin menciones
 // Reemplaza la URL con la imagen que deseas enviar
 const imageUrl = 'https://i.ibb.co/TcTRpHW/file.jpg';
-// Enviamos el mensaje con la imagen y el mensaje correspondiente
-await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: str }, { quoted: m });
+// Enviamos solo la imagen
+await conn.sendMessage(m.chat, { image: { url: imageUrl } }, { quoted: m });
 }
 }
 handler.help = ['precios1'];
-handler.tags = ['info'];
+handler.tags = ['main'];
 handler.command = ['precios1'];
 handler.group = true;
 export default handler;
