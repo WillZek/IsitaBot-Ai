21b8bd92d5c3a46f92b57cd40cab7baf56de5292
Niño Piña <ninopina10@gmail.com>
2024-12-03 13:50:25 -0600

    Actualizar plugins

---
+++ plugins/group-precios3.js
@@ -0,0 +1,20 @@
+// Código Hecho Por Niño Piña wa.me/50557865603
+let handler = async (m, { conn }) => {
+// React con un emoji al mensaje
+m.react('💫');
+// Mensaje que se enviará
+const message = "*AQUI ESTAN LOS PRECIOS.*\n\n> Renovación💫";
+if (m.isGroup) {
+// URL de la imagen
+const imageUrl = 'https://i.ibb.co/x5w5CHk/file.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
+// Envía el mensaje
+await conn.sendMessage(m.chat, { text: message }, { quoted: m });
+// Envía la imagen
+await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
+}
+}
+handler.help = ['precios3'];
+handler.tags = ['main'];
+handler.command = ['precios3'];
+handler.group = true;
+export default handler;
