3899f44a2fce27c5fcfdeab5c580801f3d668016
Niño Piña <ninopina10@gmail.com>
2024-12-03 13:03:06 -0600

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) return m.react('✖️')
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('*🧑‍💻 REINICIANDO CROWBOT-AI*')
+    await m.reply('*🧑‍💻 REINICIANDO ISITABOT-AI*')
     process.send('reset')
   } else return m.react('✖️')
 }
