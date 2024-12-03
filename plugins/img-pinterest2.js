3d2431fd84e422fb85d0dbac4327cefd3d24d291
Niño Piña <ninopina10@gmail.com>
2024-12-03 14:17:24 -0600

    Actualizar plugins

---
+++ plugins/img-pinterest2.js
@@ -0,0 +1,21 @@
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, `*💫 Uso Correcto: ${usedPrefix + command} Isita Edit*`, m, rcanal);
+conn.reply(m.chat, '💫 *Descargando su imagen...*', m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+title: packname,
+body: wm,
+previewType: 0, thumbnail: icons,
+sourceUrl: channel }}})
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `💫 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+};
+handler.help = ['pinterest2 <query>'];
+handler.tags = ['img', 'tools'];
+handler.command = ['pinterest2', 'image', 'imagen'];
+handler.register = true;
+export default handler;
