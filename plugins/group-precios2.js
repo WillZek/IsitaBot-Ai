4e05020d45d6d2f15e4c92172fe852d15c01eb0b
Niño Piña <ninopina10@gmail.com>
2024-12-03 23:32:07 -0600

    Actualizar group-precios2.js

--- plugins/group-precios2.js
+++ plugins/group-precios2.js
@@ -4,11 +4,10 @@ let handler = async (m, { conn }) => {
 m.react('💫');
 // Mensaje que se enviará
 const message = "*AQUI ESTAN LOS PRECIOS.*\n\n> Bot Para Grupos💫";
-if (m.isGroup) {
+if (m. rcanal) {
 // URL de la imagen
 const imageUrl = 'https://i.ibb.co/TcTRpHW/file.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
 // Envía el mensaje
-await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 // Envía la imagen
 await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
 }
@@ -16,5 +15,4 @@ await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, {
 handler.help = ['precios2'];
 handler.tags = ['main'];
 handler.command = ['precios2'];
-handler.group = true;
 export default handler;
