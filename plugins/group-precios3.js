dbe9cc8d0d78b37f26b11c80973b843c4021ac60
Niño Piña <ninopina10@gmail.com>
2024-12-03 23:32:34 -0600

    Actualizar group-precios3.js

--- plugins/group-precios3.js
+++ plugins/group-precios3.js
@@ -4,11 +4,10 @@ let handler = async (m, { conn }) => {
 m.react('💫');
 // Mensaje que se enviará
 const message = "*AQUI ESTAN LOS PRECIOS.*\n\n> Continuación De los Precios1💫";
-if (m.isGroup) {
+if (m. rcanal) {
 // URL de la imagen
 const imageUrl = 'https://i.ibb.co/h7MpyhP/file.jpg'; // Cambia esta URL por la de la imagen que deseas enviar
 // Envía el mensaje
-await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 // Envía la imagen
 await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, { mimetype: 'image/jpeg' });
 }
@@ -16,5 +15,4 @@ await conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: message }, {
 handler.help = ['precios3'];
 handler.tags = ['main'];
 handler.command = ['precios3'];
-handler.group = true;
 export default handler;
