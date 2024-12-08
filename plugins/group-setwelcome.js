cac51b4b10771ced8e13d3eb0c550e528a4245e8
Niño Piña <ninopina10@gmail.com>
2024-12-07 21:47:18 -0600

    Actualizar group-setwelcome.js

--- plugins/group-setwelcome.js
+++ plugins/group-setwelcome.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner, isAdmin, usedPrefix, command }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
-    m.reply('mensaje de bienvenida configurado con exito\n@user (Mencion)\n@subject (Nombre del grupo)\n@desc (Descripcio )')
+    m.reply('mensaje de bienvenida configurado con exito\n@user (Mencion)\n@subject (Nombre del grupo)\n@desc (Descripcion)')
   } else throw m.reply(`y el textl?\n\nejemplo:\n${usedPrefix + command} hola, @user!\nBienvenido al grupo @subject\n\n@desc`)
 }
 handler.help = ['setwelcome <txt>']
