536a3d61666e5a5b8d6b902d0d8545132e12de3e
Niño Piña <ninopina10@gmail.com>
2024-12-11 18:06:52 -0600

    Actualizar downloader-facebook.js

--- plugins/downloader-facebook.js
+++ plugins/downloader-facebook.js
@@ -35,7 +35,7 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
   } catch (error) {
-    return conn.reply(m.chat, '*`Error al enviar el video. ${e.message}`*', m);
+    return conn.reply(m.chat, `*Error al enviar el video. ${e.message}*`, m);
   await m.react('❌');
   }
 };
