2ff0a51e3a727d0e923cf8b6956bd0e8f423a6a9
Niño Piña <ninopina10@gmail.com>
2024-12-26 12:12:56 -0600

    Actualizar isita-setname.js

--- plugins/isita-setname.js
+++ plugins/isita-setname.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) return m.reply('🌐 Por favor, proporciona un nombre para el bot.\nEjemplo: setname IsitaBot');
 
-  global.botname = text.trim();
+  global.namebot = text.trim();
 
-  m.reply(`✐ El nombre del bot ha sido cambiado a: ${global.botname}`);
+  m.reply(`✐ El nombre del bot ha sido cambiado a: ${global.namebot}`);
 };
 
 handler.help = ['setname'];
