e731bfa9826986995d7c1fe503011afdebd438e0
Niño Piña <ninopina10@gmail.com>
2024-12-26 12:19:51 -0600

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -27,7 +27,7 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
       txt += `│  ✩  *EDAD* : ${age} años\n`
       txt += `│  ✩  *NUMERO DE SERIE*\n`
       txt += `│  ✩  ${sn}
-      txt += `└  ✩  *BOT* : ${namebot}`
+      txt += `└  ✩  ${namebot}`
 await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
 await m.react('✅')
 }
