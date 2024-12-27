d448ae2a31c22eeba242ceedd37a1c50589132ee
Niño Piña <ninopina10@gmail.com>
2024-12-27 14:32:31 -0600

    Actualizar img-pinterest2.js

--- plugins/img-pinterest2.js
+++ plugins/img-pinterest2.js
@@ -1,21 +1,18 @@
-import {googleImage} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-if (!text) return conn.reply(m.chat, `*💫 Uso Correcto: ${usedPrefix + command} Isita Edit*`, m, rcanal);
-conn.reply(m.chat, '💫 *Descargando su imagen...*', m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icono,
-sourceUrl: 'https://www.instagram.com/star.moony_desing' }}})
-const res = await googleImage(text);
-const image = await res.getRandom();
-const link = image;
-const messages = [['Imagen 1', dev, await res.getRandom(),
-[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
-await conn.sendCarousel(m.chat, `💫 Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
-};
-handler.help = ['pinterest2 <query>'];
-handler.tags = ['img', 'tools'];
-handler.command = ['pinterest2', 'image', 'imagen'];
-handler.register = true;
-export default handler;
+import Starlights from "@StarlightsTeam/Scraper"
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('🚩 Ingresa el nombre de la imágen que estas buscando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Elaina Icons`)
+await m.react('🕓')
+try {
+let { dl_url } = await Starlights.pinterest(text)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', `*» Resultado* : ${text}`, m, null, rcanal)
+await m.react('✅')
+} catch {
+await m.react('✖️')
+}}
+handler.help = ['pinterest *<búsqueda>*']
+handler.tags = ['img']
+handler.command = ['pinterest']
+handler.register = true 
+//handler.estrellas = 3;
+export default handler
