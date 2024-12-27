1117599aec9ca2a0f52cd31976bd6c7cb35f606f
Niño Piña <ninopina10@gmail.com>
2024-12-27 14:05:45 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -203,11 +203,11 @@ global.rcanal = {
     forwardedNewsletterMessageInfo: {
       newsletterJid: "https://www.instagram.com/star.moony_desing",
       serverMessageId: 100,
-      newsletterName: name,
+      newsletterName: 'Channel Oficial',
    }, 
    externalAdReply: {
     showAdAttribution: true, 
-    title: name2, 
+    title: 'IsitaBot-Channel', 
     body: textbot, 
     mediaUrl: null, 
     description: null, 
@@ -221,7 +221,7 @@ global.rcanal = {
 
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null)
-   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, estilo)
+   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, rcanal, estilo)
 
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
