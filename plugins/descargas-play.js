5bddd2fe434005ecb1b88e7094955eb23d623f86
Niño Piña <ninopina10@gmail.com>
2025-01-02 10:30:09 -0600

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -53,7 +53,7 @@ let handler = async (m, { conn, args }) => {
 
 handler.help = ['play *<texto>*'];
 handler.tags = ['dl'];
-handler.command = ['play'];
+handler.command = ['playop'];
 
 export default handler;
 
