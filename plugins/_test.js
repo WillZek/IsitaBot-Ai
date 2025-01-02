f0c2df7a3c6ae2607117cf304ea3683790b7ed6a
Niño Piña <ninopina10@gmail.com>
2025-01-02 11:53:21 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,4 +1,6 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+let banner = 'https://i.ibb.co/s9N9QhG/file.jpg'
+
   let txt = `Nose?`
   conn.sendMessage(m.chat, {
     image: banner,
