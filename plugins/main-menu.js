71e7a2664e6467f68e825e638f7914450b25c616
Niño Piña <ninopina10@gmail.com>
2025-01-02 10:55:30 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -229,8 +229,27 @@ global.rcanal = {
     }
 
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
-    await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null)
-   //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, rcanal, estilo)
+    await conn.sendFile(m.chat, {
+      image: img,
+      caption: txt,
+      footer: 'Selecciona una opción',
+      buttons: [
+        {
+          buttonId: `.s`,
+          buttonText: {
+            displayText: 'Probando',
+          },
+        },
+        {
+          buttonId: `.menu3`,
+          buttonText: {
+            displayText: 'Probando2',
+          },
+        },
+      ],
+      viewOnce: true,
+      headerType: 4,
+    }, { quoted: m });
 
   } catch (e) {
     conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
