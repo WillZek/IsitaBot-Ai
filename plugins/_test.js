45f751be051a850bc47bf575b452f5ef671ac617
Niño Piña <ninopina10@gmail.com>
2025-01-02 12:01:34 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,27 +1,27 @@
-let handler = async (m, { conn, args,
-usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   let banner = 'https://i.ibb.co/s9N9QhG/file.jpg';
   let txt = `Nose?`;
-  
-  conn.sendFile(m.chat, banner, txt, "1234", m, {
+
+  const buttons = [
+    {
+      buttonId: ".menu",
+      buttonText: { displayText: 'test' }
+    },
+    {
+      buttonId: ".s",
+      buttonText: { displayText: "Hola" }
+    }
+  ];
+
+  const buttonMessage = {
+    image: { url: banner },
+    caption: txt,
     footer: botname,
-    buttons: [
-      {
-        buttonId: ".menu",
-        buttonText: {
-          displayText: 'test'
-        }
-      },
-      {
-        buttonId: ".s",
-        buttonText: {
-          displayText: "Hola"
-        }
-      }
-    ],
-    viewOnce: true,
+    buttons: buttons,
     headerType: 1,
-  });
+  };
+
+  await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
 }
 
 handler.command = ['test'];
