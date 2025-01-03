7a33ffd0b1804aeae5e5c2c68b19036c4e9be32d
Niño Piña <ninopina10@gmail.com>
2025-01-02 22:39:01 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -2,27 +2,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let banner = 'https://i.ibb.co/s9N9QhG/file.jpg';
   let txt = `Nose?`;
 
-  const buttons = [
-    {
-      buttonId: ".menu",
-      buttonText: { displayText: 'test' }
-    },
-    {
-      buttonId: ".s",
-      buttonText: { displayText: "Hola" }
-    }
-  ];
-
-  const buttonMessage = {
-    image: { url: banner },
-    caption: txt,
-    footer: botname,
-    buttons: buttons,
-    headerType: 1,
-  };
-
-  await conn.sendMessage(m.chat, buttonMessage, { quoted: m });
-}
+ sock.sendMessage(m.from, { image: await m.quoted.download(), caption: "nazi bot muaaajjjj", footer: "by nazi", buttons: [{buttonId: "$ echo hola gei", buttonText: { displayText: "hola" }}, {buttonId: "$ echo adios gei", buttonText: { displayText: "bye" }}], headerType: 6, viewOnce: true, contextInfo: {forwardedNewsletterMessageInfo: { newsletterJid: '120363382214376298@newsletter', serverMessageId: '', newsletterName: 'Test Bot'},isForwarded: true}}, { quoted: vCard })
 
 handler.command = ['test'];
 export default handler;
