8c80cb83c41e72a368d692fb3cda0f406f450492
Niño Piña <ninopina10@gmail.com>
2025-01-03 10:38:19 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -1,12 +1,33 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let banner = 'https://i.ibb.co/s9N9QhG/file.jpg';
-  let txt = `Nose?`;
+  try {
+    let banner = 'https://i.ibb.co/s9N9QhG/file.jpg';
+    let txt = `Nose?`;
 
- conn.sendMessage(m.from, { image: banner, caption: "nazi bot muaaajjjj", footer: "by nazi", buttons: [{buttonId: "$ echo hola gei", buttonText: { displayText: "hola" }}, {buttonId: "$ echo adios gei", buttonText: { displayText: "bye" }}], headerType: 6, viewOnce: true, contextInfo: {forwardedNewsletterMessageInfo: { newsletterJid: '120363382214376298@newsletter', serverMessageId: '', newsletterName: 'Test Bot'},isForwarded: true}}, { quoted: m })
+    conn.sendMessage(m.from, { 
+      image: banner, 
+      caption: "nazi bot muaaajjjj", 
+      footer: "by nazi", 
+      buttons: [
+        { buttonId: "$ echo hola gei", buttonText: { displayText: "hola" } }, 
+        { buttonId: "$ echo adios gei", buttonText: { displayText: "bye" } }
+      ], 
+      headerType: 6, 
+      viewOnce: true, 
+      contextInfo: {
+        forwardedNewsletterMessageInfo: { 
+          newsletterJid: '120363382214376298@newsletter', 
+          serverMessageId: '', 
+          newsletterName: 'Test Bot' 
+        }, 
+        isForwarded: true
+      }
+    }, { quoted: m });
 
-} catch (error) {
-m.reply(`Error: ${error.message}`);
-m.react('✖️');
+  } catch (error) {
+    m.reply(`Error: ${error.message}`);
+    m.react('✖️');
+  }
+}
 
 handler.command = ['test'];
 export default handler;
