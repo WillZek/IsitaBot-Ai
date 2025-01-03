cc9ccd9dadbfacf2724662e6aecb581b05a3f630
Niño Piña <ninopina10@gmail.com>
2025-01-02 22:58:07 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -4,5 +4,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
  conn.sendMessage(m.from, { image: banner, caption: "nazi bot muaaajjjj", footer: "by nazi", buttons: [{buttonId: "$ echo hola gei", buttonText: { displayText: "hola" }}, {buttonId: "$ echo adios gei", buttonText: { displayText: "bye" }}], headerType: 6, viewOnce: true, contextInfo: {forwardedNewsletterMessageInfo: { newsletterJid: '120363382214376298@newsletter', serverMessageId: '', newsletterName: 'Test Bot'},isForwarded: true}}, { quoted: m })
 
+} catch (error) {
+m.reply(`Error: ${error.message}`);
+m.react('✖️');
+
 handler.command = ['test'];
 export default handler;
