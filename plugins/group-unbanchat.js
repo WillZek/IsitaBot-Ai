54ab175caedcf40947b2e46d438428a6962585c9
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-10 12:20:44 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,12 +1,11 @@
-let handler = async (m, { conn, isAdmin, isROwner, args } ) => {
+let handler = async (m, { conn, isAdmin, isROwner} ) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    if (args[0] == 'on') {
     global.db.data.chats[m.chat].isBanned = false
-    await conn.reply(m.chat, '🌟 IsitaBot activo en este grupo.', m, rcanal)
+    await conn.reply(m.chat, '🚩 Bot activo en este grupo.', m, rcanal)
     await m.react('✅')
 }
 handler.help = ['desbanearbot']
 handler.tags = ['group']
-handler.command = ['isita', 'unbanchat']
+handler.command = ['desbanearbot', 'unbanchat']
 handler.group = true 
 export default handler
