722fc8220c1db3882b8fba926bd0548cf708a89f
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-14 16:21:31 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,11 @@
-let handler = async (m, { conn, isAdmin, isROwner }) => {
-    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
-    global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `💫ISITABOT FUE DESACTIVADO EN ESTE CHAT`, m, rcanal)
-    await m.react('✅')
+let handler = async (m) => {
+
+global.db.data.chats[m.chat].isBanned = true
+conn.reply(m.chat, `✅ *El Bot Ha Sido Desactivado En Este Chat*`, m, rcanal)
+
 }
-handler.help = ['banearbot']
-handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
-handler.group = true 
+handler.help = ['banchat']
+handler.tags = ['owner']
+handler.command = /^banchat$/i
+handler.mods = true 
 export default handler
