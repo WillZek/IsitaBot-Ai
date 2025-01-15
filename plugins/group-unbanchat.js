b888c05780ae5a4170300653bcc9d0ef18898aad
【⚡︎𝑾𝒊𝒍𝒍𝒁𝒆𝒌 - 𝑺.𝑪.𝑨࿐】 <ninopina10@gmail.com>
2025-01-15 11:57:26 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🎌 *¡Este chat no está registrado!*', m)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '[🌠] *El bot no está baneado en este chat*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '[🌠] *El bot no está baneado en este chat*', m)
 chat.isBanned = false
-await conn.reply(m.chat, `🍭 *IsitaBot ya fué desbaneado en este chat*`, m, fake)
+await conn.reply(m.chat, `🍭 *IsitaBot ya fué desbaneado en este chat*`, m)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
