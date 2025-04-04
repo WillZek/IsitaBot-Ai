24c2f391bc4f1a85b02b9c6e37af2f1ae68792db
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-04 17:30:26 -0600

    Actualizar _top3.js

--- plugins/_top3.js
+++ plugins/_top3.js
@@ -1,4 +1,4 @@
-let handler = async(m, { groupMetadata, command, conn, usedPrefix, participants }) => {
+let handler = async(m, { groupMetadata, command, conn, usedPrefix }) => {
 let user = a => '@' + a.split('@')[0]
 let ps = groupMetadata.participants.map(v => v.id)
 let a = ps.getRandom()
