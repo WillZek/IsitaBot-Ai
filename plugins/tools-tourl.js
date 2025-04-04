9cc9d0dfdef1c43db58bb5872b127501576cd056
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-04 15:05:46 -0600

    Actualizar tools-tourl.js

--- plugins/tools-tourl.js
+++ plugins/tools-tourl.js
@@ -3,60 +3,57 @@ import crypto from "crypto";
 import { FormData, Blob } from "formdata-node";
 import { fileTypeFromBuffer } from "file-type";
 
-const handler = async (m, { conn }) => {
-let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || "";
-  if (!mime) return m.reply("No media found", null, { quoted: fkontak });
-  let media = await q.download();
-let link = await catbox(media);
-  let caption = `📮 *L I N K :*
- \`\`\`• ${link}\`\`\`
-📊 *S I Z E :* ${formatBytes(media.length)}
-📛 *E x p i r e d :* "No Expiry Date" 
-`;
+let handler = async (m, { conn }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) return conn.reply(m.chat, `🍫 Por favor, responde a un archivo válido (imagen, video, etc.).`, m);
 
-  await m.reply(caption);
-}
-handler.command = handler.help = ['tourl']
-handler.tags = ['tools']
-handler.diamond = true
-export default handler
+  await m.react(rwait);
 
+  try {
+    let media = await q.download();
+    let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime);
+    let { link, name } = await megaUpload(media);
+
+    let txt = `*乂 M E G A - U P L O A D E R 乂*\n\n`;
+    txt += `*» Enlace* : ${link || 'No disponible'}\n`;
+    txt += `*» Nombre* : ${name}\n`;
+    txt += `*» Tamaño* : ${formatBytes(media.length)}\n`;
+    txt += `*» Expiración* : ${isTele ? 'No expira' : 'Desconocido'}\n\n`;
+    txt += `> *${dev}*`;
+
+    await conn.sendFile(m.chat, media, 'thumbnail.jpg', txt, m, fkontak);
+
+    await m.react(done);
+  } catch {
+    await m.react(error);
+  }
+};
+
+handler.help = ['up'];
+handler.tags = ['transformador'];
+handler.command = ['up', 'to', 'tourl'];
+
+export default handler;
 
 function formatBytes(bytes) {
   if (bytes === 0) {
-    return "0 B";
+    return '0 B';
   }
-  const sizes = ["B", "KB", "MB", "GB", "TB"];
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
   const i = Math.floor(Math.log(bytes) / Math.log(1024));
   return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
 }
 
-
-/**
- * Upload image to catbox
- * Supported mimetype:
- * - `image/jpeg`
- * - `image/jpg`
- * - `image/png`s
- * - `image/webp`
- * - `video/mp4`
- * - `video/gif`
- * - `audio/mpeg`
- * - `audio/opus`
- * - `audio/mpa`
- * @param {Buffer} buffer Image Buffer
- * @return {Promise<string>}
- */
-async function catbox(content) {
+async function megaUpload(content) {
   const { ext, mime } = (await fileTypeFromBuffer(content)) || {};
-  const blob = new Blob([content.toArrayBuffer()], { type: mime });
+  const blob = new Blob([content], { type: mime });
   const formData = new FormData();
   const randomBytes = crypto.randomBytes(5).toString("hex");
   formData.append("reqtype", "fileupload");
   formData.append("fileToUpload", blob, randomBytes + "." + ext);
 
-  const response = await fetch("https://catbox.moe/user/api.php", {
+  const response = await fetch("https://cdnmega.vercel.app/upload", {
     method: "POST",
     body: formData,
     headers: {
@@ -65,5 +62,11 @@ async function catbox(content) {
     },
   });
 
-  return await response.text();
+  const result = await response.json();
+
+  if (result.success && result.files.length > 0) {
+    return { link: result.files[0].url, name: randomBytes + "." + ext };
+  } else {
+    return { link: null, name: randomBytes + "." + ext };
+  }
 }
